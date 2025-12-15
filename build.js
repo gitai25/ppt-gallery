8576aed26ab9f4aac76288df05f4465919473625
AI+2025 <ai2025@AI2025dexuniji.local>
2025-12-15 16:33:27 +0800

    Add favicon

--- build.js
+++ build.js
@@ -75,6 +75,7 @@ function generateHTML(ppts) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PPT Gallery</title>
+  <link rel="icon" type="image/svg+xml" href="favicon.svg">
   <style>
     * { margin: 0; padding: 0; box-sizing: border-box; }
 
